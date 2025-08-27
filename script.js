de52a8e69be668c5b0c0045e357b42451c2582d2
Iserte <iserte@bisonic.io>
2025-08-27 16:56:59 -0700

    Create movies list

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ console.log("Year:", year);
 console.log("Is student:", isStudent);
 console.log("Empty value:", emptyValue);
 console.log("No value:", noValue);
+
+const movies = [
+  "Inception",
+  "Spirited Away",
+  "The Matrix",
+  "Interstellar",
+  "Parasite",
+];
+const moviesList = document.createElement("ul");
+movies.forEach((movie) => {
+  const li = document.createElement("li");
+  li.textContent = movie;
+  moviesList.appendChild(li);
+});
+document.body.appendChild(moviesList);
