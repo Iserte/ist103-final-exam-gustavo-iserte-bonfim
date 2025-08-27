f1cb0c701f16ac29c5d49c13869d07fccb27171f
Iserte <iserte@bisonic.io>
2025-08-27 16:53:35 -0700

    Define variables of all basic data types

---
+++ script.js
@@ -0,0 +1,11 @@
+let userName = "Gustavo";
+const year = 2025;
+var isStudent = true;
+let emptyValue = null;
+let noValue;
+
+console.log("Name:", userName);
+console.log("Year:", year);
+console.log("Is student:", isStudent);
+console.log("Empty value:", emptyValue);
+console.log("No value:", noValue);
